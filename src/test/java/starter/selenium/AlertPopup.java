c46c335a0fb9ed418bc757c1cc55eaf23c08fdca
Luis Perez <luis.perez@SOMO-M-MED-038.local>
2022-01-26 20:25:08 -0500

    desired capabilities

--- src/test/java/starter/selenium/AlertPopup.java
+++ src/test/java/starter/selenium/AlertPopup.java
@@ -8,6 +8,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.Iterator;
+import java.util.Set;
+
 public class AlertPopup {
     WebDriver driver;
 
@@ -39,6 +42,40 @@ public class AlertPopup {
         alert.accept();
     }
 
+    @Test
+    public void testMultipleAlerts(){
+        driver.get("http://demo.guru99.com/popup.php");
+        driver.manage().window().maximize();
+
+        driver.findElement(By.xpath("//*[contains(@href,'popup.php')]")).click();
+
+        String MainWindow=driver.getWindowHandle();
+
+        // To handle all new opened window.
+        Set<String> s1=driver.getWindowHandles();
+        Iterator<String> i1=s1.iterator();
+
+        while(i1.hasNext())
+        {
+            String ChildWindow=i1.next();
+
+            if(!MainWindow.equalsIgnoreCase(ChildWindow))
+            {
+
+                // Switching to Child window
+                driver.switchTo().window(ChildWindow);
+                driver.findElement(By.name("emailid"))
+                        .sendKeys("gaurav.3n@gmail.com");
+
+                driver.findElement(By.name("btnLogin")).click();
+
+                // Closing the Child Window.
+                driver.close();
+            }
+        }
+        // Switching to Parent window i.e Main Window.
+        driver.switchTo().window(MainWindow);
+    }
     @After
     public void closeDriver(){
         driver.close();
